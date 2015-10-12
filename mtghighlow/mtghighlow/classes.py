304c0210315ee5f117a8d822f6aea31afc8c2391
Shawn Tabrizi <shawntabrizi@gmail.com>
2015-10-12 01:17:01 -0700

    Changed Result Strings, removed dollar sign from fakeprice variable

--- mtghighlow/mtghighlow/classes.py
+++ mtghighlow/mtghighlow/classes.py
@@ -1,4 +1,4 @@
-from scraper import getCFBPrice, getGoldfishTopCards, getCardImageURL
+﻿from scraper import getCFBPrice, getGoldfishTopCards, getCardImageURL
 import random
 
 class Streak:
@@ -20,17 +20,17 @@ class Streak:
 
         if result == "Higher":
             if self.lastcard.fakeprice > self.lastcard.realprice:
-                correct = "Incorrect"
+                correct = "Incorrect: The real price is LOWER"
                 self.streak = 0
             elif self.lastcard.fakeprice < self.lastcard.realprice:
-                correct = "Correct"
+                correct = "Correct: The real price is HIGHER"
                 self.streak += 1
         elif result == "Lower":
             if self.lastcard.fakeprice < self.lastcard.realprice:
-                correct = "Incorrect"
+                correct = "Incorrect: The real price is HIGHER"
                 self.streak = 0
             elif self.lastcard.fakeprice > self.lastcard.realprice:
-                correct = "Correct"
+                correct = "Correct: The real price is LOWER"
                 self.streak += 1
         if self.beststreak < self.streak:
             self.beststreak = self.streak
@@ -51,6 +51,6 @@ class Card:
             self.fakeprice = fakeprice
         else:
             fakepricefloat = float(self.realprice[1:]) + float(self.realprice[1:]) * .01 * random.randrange(-90,100)
-            self.fakeprice = '${:0,.2f}'.format(fakepricefloat)
+            self.fakeprice = '{:0,.2f}'.format(fakepricefloat)
 
         self.image = getCardImageURL(cardname, None)[0]
