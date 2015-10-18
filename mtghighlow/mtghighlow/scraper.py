e06faa83f986f1b52ec787442c22b1a2d9c0eae7
Shawn Tabrizi <shawntabrizi@gmail.com>
2015-10-18 15:47:50 -0700

    Removed uneeded scraper functions, added a text replace to catch mtggoldfish sending 'prm-gpp' and changing it to GPX which magiccards.info understands

--- mtghighlow/mtghighlow/scraper.py
+++ mtghighlow/mtghighlow/scraper.py
@@ -25,7 +25,9 @@ def getGoldfishFormatCards(format,paper):
         for value in (values[0], values[1], values[3]):
             thestrings = [unicode(s) for s in value.findAll(text=True)]
             thetext = ''.join(thestrings)
-            a[-1].append(thetext.replace('\n',''))
+            thetext.replace('\n','')
+            thetext.replace('PRM-GPP','GPX')
+            a[-1].append(thetext)
         if index > 100:
             break
     return a
@@ -58,69 +60,3 @@ def getCardImageURL(cardName, cardSet):
     endURLIndex = rawHTML.find("\"", startURLIndex)
     imageURL = rawHTML[startURLIndex:endURLIndex]
     return [imageURL]
-
-#
-#   Retrieves a cards current price on Channel Fireball
-#
-def getCFBPrice(cardName, cardSet):
-    cfbURL = "http://store.channelfireball.com/products/search?q=" + urllib.quote(cardName)
-    if cardSet:
-        cfbURL += " " + urllib.quote(cardSet)
-    htmlFile = urllib.urlopen(cfbURL)
-    rawHTML = htmlFile.read()    
-    tempIndex = rawHTML.find("grid-item-price")
-    startPriceIndex = rawHTML.find("$", tempIndex)
-    endPriceIndex = rawHTML.find("<", startPriceIndex)
-    cfbPrice = rawHTML[startPriceIndex+1:endPriceIndex]
-    return [cfbPrice]
-
-#
-#   Retrieves the lowest buy it now price for a card on ebay
-#
-def getEbayPrice(cardName, cardSet):
-    ebayURL = "http://www.ebay.com/sch/i.html?_sacat=0&_sop=15&LH_BIN=1&_nkw=" + urllib.quote(cardName)
-    if cardSet:
-        ebayURL += urllib.quote(" " + cardSet)
-    ebayURL += urllib.quote( " mtg nm")
-    logging.info(ebayURL)
-    htmlFile = urllib.urlopen(ebayURL)
-    rawHTML = htmlFile.read()
-    startPriceIndex = rawHTML.find('span  class="g-b">')
-    startPriceIndex = rawHTML.find("$", startPriceIndex)
-    endPriceIndex = rawHTML.find("<", startPriceIndex)
-    lowestBIN = rawHTML[startPriceIndex:endPriceIndex]
-    return [lowestBIN]
-
-#
-#   Retrieves the low, mid, and high prices of a card as shown on http://tcgplayer.com
-#
-def getTCGPlayerPrices(cardName, cardSet):
-    #   Open the TCGPlayer URL
-    tcgPlayerURL = "http://magic.tcgplayer.com/db/magic_single_card.asp?cn=" + urllib.quote(cardName)
-    if cardSet:
-       tcgPlayerURL += "&sn=" + urllib.quote(cardSet)
-    htmlFile = urllib.urlopen(tcgPlayerURL)
-    rawHTML = htmlFile.read()
-
-    #   Scrape for the low price
-    tempIndex = rawHTML.find('>L:')
-    startLowIndex = rawHTML.find("$", tempIndex)
-    endLowIndex = rawHTML.find("<", startLowIndex)
-
-    lowPrice = rawHTML[startLowIndex:endLowIndex]
-
-    #   Scrape for the mid price
-    tempIndex = rawHTML.find('>M:')
-    startMidIndex = rawHTML.find("$", tempIndex)
-    endMidIndex = rawHTML.find("<", startMidIndex)
-    
-    midPrice = rawHTML[startMidIndex:endMidIndex]
-
-    #   Scrape for the high price
-    tempIndex = rawHTML.find('>H:')
-    startHighIndex = rawHTML.find("$", tempIndex)
-    endHighIndex = rawHTML.find("<", startHighIndex)
-    
-    highPrice = rawHTML[startHighIndex:endHighIndex]
-
-    return [lowPrice, midPrice, highPrice]
