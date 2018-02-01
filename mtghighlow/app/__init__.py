d17a407e6e97ed619e150f45430f83415b524074
Richard Neal <richard@neal.in>
2018-02-01 00:38:42 -0800

    Change directories

--- mtghighlow/app/__init__.py
+++ mtghighlow/app/__init__.py
@@ -6,4 +6,4 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'e\xcb2`\xaf*]\x9c\x10o\x8b\xe7\xd7\x1a\xbb\xd4\xb9\x89\xc8H76h.'
 
-import mtghighlow.views
+import ..mtghighlow.views
